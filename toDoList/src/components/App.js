b9050bed2df7746bc64f929eae51b71c29e8ef10
Nimboo1 <qwertyuniverse12@yandex.ru>
2023-09-17 21:11:26 +0300

    feat: add all functionality

--- toDoList/src/components/App.js
+++ toDoList/src/components/App.js
@@ -1,14 +1,19 @@
-// import listElement from './listElement';
+import listElement from './listElement';
+import removeClasses from '../utils/removeClasses';
 
 class App {
-  _elementList;
+  _uncomplitedList;
   _complitedList;
-  _textList;
   _root;
+  _elementCount;
+  _elementList;
 
   constructor() {
     this._root = document.createElement('div');
     this._root.id = 'app';
+    this._elementList = [];
+
+    this._elementCount = Math.max(...Object.keys(localStorage)) < 0 ? 0 : Math.max(...Object.keys(localStorage));
   }
 
   init() {
@@ -16,9 +21,67 @@ class App {
   }
 
   buildApp() {
+    const addItem = () => {
+      removeClasses();
+
+      const text = this.input.value;
+      if (text === '') return;
+
+      this.input.value = '';
+      this._elementCount++;
+
+      const element = new listElement(text, this._elementCount);
+      this._uncomplitedList.append(element.getElement());
+      localStorage.setItem(this._elementCount, JSON.stringify([text, 1]));
+    };
+    const setEven = () => {
+      this._elementList = document.querySelectorAll('.list-element');
+      for (let i = 0; i < this._elementList.length; i++) {
+        if (i % 2 === 0) {
+          this._elementList[i].classList.toggle('even');
+        }
+      }
+    };
+    const setOdd = () => {
+      this._elementList = document.querySelectorAll('.list-element');
+      for (let i = 0; i < this._elementList.length; i++) {
+        if (i % 2 !== 0) {
+          this._elementList[i].classList.toggle('odd');
+        }
+      }
+    };
+    const deleteFirst = () => {
+      removeClasses();
+
+      const element = document.querySelectorAll('.list-element');
+      if (element.length === 0) return;
+      element[0].remove();
+      localStorage.removeItem(element[0].dataset.id);
+    };
+    const deleteLast = () => {
+      removeClasses();
+
+      const element = document.querySelectorAll('.list-element');
+      if (element.length === 0) return;
+      element[element.length - 1].remove();
+      localStorage.removeItem(element[element.length - 1].dataset.id);
+    };
+
     const header = document.createElement('div');
     header.classList.add('header');
     header.innerText = 'TO DO LIST';
+
+    const inputBlock = document.createElement('div');
+    inputBlock.classList.add('input-block');
+    this.input = document.createElement('input');
+    this.input.type = 'text';
+    this.input.addEventListener('keyup', (e) => {
+      if (e.key === 'Enter') {
+        addItem();
+      }
+    });
+
+    inputBlock.append(this.input, createButton('Add', addItem));
 
     const settings = document.createElement('div');
     settings.classList.add('settings');
@@ -26,22 +89,31 @@ class App {
     settings.append(
       createButton('Even', setEven),
       createButton('Odd', setOdd),
-      createButton('Delete last', deleteLast),
-      createButton('Delete first', deleteFirst)
+      createButton('Delete first', deleteFirst),
+      createButton('Delete last', deleteLast)
     );
 
-    this._elementList = document.createElement('ul');
-    this._elementList.classList.add('element-list');
+    this._uncomplitedList = document.createElement('ul');
+    this._uncomplitedList.classList.add('element-list');
 
     this._complitedList = document.createElement('ul');
     this._complitedList.classList.add('element-list', 'complited-list');
 
-    this._root.append(header, settings, this._elementList, this._complitedList);
+    const keys = Object.keys(localStorage).sort((a, b) => a - b);
+    for (let key of keys) {
+      const text = JSON.parse(localStorage.getItem(key))[0];
+      const index = JSON.parse(localStorage.getItem(key))[1];
+      const element = new listElement(text, key).getElement();
 
-    function setEven() {}
-    function setOdd() {}
-    function deleteLast() {}
-    function deleteFirst() {}
+      if (index === 1) {
+        this._uncomplitedList.append(element);
+      } else {
+        element.classList.add('complited');
+        this._complitedList.append(element);
+      }
+    }
+
+    this._root.append(header, inputBlock, settings, this._uncomplitedList, this._complitedList);
 
     function createButton(name, event) {
       const button = document.createElement('button');
@@ -53,7 +125,5 @@ class App {
 
     return this._root;
   }
-
-  createSettings() {}
 }
 export default App;
--- toDoList/src/components/listElement.js
+++ toDoList/src/components/listElement.js
@@ -1,13 +1,48 @@
-// class listElement {
-//   constructor(text) {
-//     this._element = document.createElement('div');
-//   }
+import removeClasses from '../utils/removeClasses';
 
-//   getElement() {
-//     return this._element;
-//   }
+class listElement {
+  constructor(text, id) {
+    this._element = document.createElement('div');
+    this._element.classList.add('list-element');
+    this._element.dataset.id = id;
+    this.id = id;
+    this._text = text;
 
-//   createCompliteButton() {}
-//   createDeleteButton() {}
-// }
-// export default listElement;
+    const p = document.createElement('p');
+    p.innerText = text;
+
+    const setComplete = () => {
+      removeClasses();
+
+      this._element.classList.add('complited');
+      const complitedList = document.querySelector('.complited-list');
+      complitedList.append(this._element);
+      localStorage.setItem(this.id, JSON.stringify([text, 2]));
+    };
+    const setDeleted = () => {
+      removeClasses();
+
+      this._element.remove();
+      localStorage.removeItem(this.id);
+    };
+
+    const compliteButton = this.createButton('complete', setComplete);
+    compliteButton.classList.add('complete-btn');
+    const deleteButton = this.createButton('delete', setDeleted);
+
+    this._element.append(compliteButton, p, deleteButton);
+  }
+
+  getElement() {
+    return this._element;
+  }
+
+  createButton(name, event) {
+    const button = document.createElement('button');
+    button.innerText = name;
+    button.addEventListener('click', event);
+
+    return button;
+  }
+}
+export default listElement;
